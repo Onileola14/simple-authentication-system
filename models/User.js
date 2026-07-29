09cc48ee1cec78be2fc3e74ca4e28b3b00c5d90c
onileola <onileola14@gmail.com>
2026-07-29 15:45:55 +0100

    modified:   models/User.js 	modified:   utils/createTokenUser.js

--- models/User.js
+++ models/User.js
@@ -35,4 +35,9 @@ UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+UserSchema.method.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+
 module.exports = moongose.model("User", UserSchema);
