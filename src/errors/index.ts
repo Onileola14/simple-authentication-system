f57df034ef448ee61073f1723dba9bbd193841b1
onileola <onileola14@gmail.com>
2026-08-18 23:25:43 +0100

    modified:   src/errors/index.ts 	modified:   src/errors/not-found.ts 	modified:   src/errors/unauthenticated.ts 	modified:   src/errors/unauthorized.ts

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,9 +1,11 @@
-const CustomAPIError = require('./custom-api');
-const UnauthenticatedError = require('./unauthenticated');
-const NotFoundError = require('./not-found');
-const BadRequestError = require('./bad-request');
-const UnauthorizedError = require('./unauthorized');
-module.exports = {
+
+import { CustomAPIError } from './custom-api';
+import { UnauthenticatedError } from './unauthenticated';
+import { NotFoundError } from './not-found';
+import { BadRequestError } from './bad-request';
+import { UnauthorizedError } from './unauthorized';
+
+export = {
   CustomAPIError,
   UnauthenticatedError,
   NotFoundError,
