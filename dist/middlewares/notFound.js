7f4b40e2e1c005e87d41e53ad55120d4cab77ce2
onileola <onileola14@gmail.com>
2026-08-18 23:18:03 +0100

    modified:   dist/middlewares/error-handler.js

--- dist/middlewares/notFound.js
+++ dist/middlewares/notFound.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.notFound = void 0;
 const notFound = (req, res) => {
     res.status(404).send("Route does not exist");
 };
-module.exports = notFound;
+exports.notFound = notFound;
 //# sourceMappingURL=notFound.js.map
